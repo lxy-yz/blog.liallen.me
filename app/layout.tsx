55f1b1c854cd8a561dd74a42543ca15bdb1d9485
lxy-yz <lxy.dev.yz@gmail.com>
2026-07-13 01:59:13 -0600

    Avoid font preload headers during export

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { SiteTabs } from "./site-tabs";
 import "./globals.css";
 
 const gaMeasurementId = "G-8EV4KNV3LN";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.SITE_URL || "https://blog.liallen.me"),
@@ -35,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
